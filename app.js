f7c312852bd694da9af856b8f02df856309a6419
sailee1 <saiclee1986@googlemail.com>
2022-06-16 13:25:40 +0100

    register user

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const dotenv= require ('dotenv')
 const colors = require ('colors') 
+const flash = require('connect-flash')
+const session = require('express-session')
 const connectDB = require('./config/db')
 const port = process.env.PORT ||  5000
 
@@ -17,6 +19,26 @@ app.set('view engine', 'ejs')
 
 //Bodyparser 
 app.use(express.urlencoded({extended: false}))
+
+
+// Express session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+  );
+
+  //Connect Flash 
+  app.use(flash())
+
+  //Global Vars 
+  app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    next()
+  })
 
 //Routes 
 app.use('/', require('./routes/index'))
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router() 
+const bcrypt = require('bcryptjs')
 
 //User model 
 const User = require('../models/User')
@@ -42,7 +43,43 @@ router.post('/register', (req, res) => {
         })
     } else{ 
        //Validation passed
+        User.findOne({email: email})
+        .then(user => {
+            if(user){
+                //User exists
+                errors.push({msg: 'Email is already registered'})
+                res.render('register', {
+                    errors, 
+                    name, 
+                    email,
+                    password, 
+                    password2
+        
+                }) 
+            } else {
+                const newUser = new User({
+                    name, 
+                    email,
+                    password
+                })
 
+                //Hash password 
+                bcrypt.genSalt(10,(err,salt) => 
+                bcrypt.hash(newUser.password, salt, (err,hash) => {
+                    if(err) throw err 
+                    //Set password to hashed
+                    newUser.password = hash
+                    //Save user 
+                    newUser.save()
+                    .then(user => {
+                        req.flash('success_msg', 'You are now registered and can log in')
+                        res.redirect('/users/login')
+                    })
+                    .catch(err => console.log(err))
+
+                }))
+            }
+        })
     }
 })
 
